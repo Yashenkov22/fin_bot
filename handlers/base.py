b5d5e45ffc62302c3b4b3ba8e05d741dae25d82d
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-07-03 11:37:02 +0300

    fix: edit has_delayed_task

--- handlers/base.py
+++ handlers/base.py
@@ -321,6 +321,8 @@ async def callback_send_order(callback: types.Message | types.CallbackQuery,
     # )
 
     async with session as _session:
+        _session.add(new_order)
+        await session.flush()
         await _session.flush()
         new_order_id = new_order.id
         # await _session.execute(query)
